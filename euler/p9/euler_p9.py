c2deb2aea0e68c5d7ff048093c06082f9a3204ac
menshikow <adriaanmenschikow@gmail.com>
2026-06-24 23:10:55 +0200

    [euler] solved 8, added some infos for 9th

---
+++ euler/p9/euler_p9.py
@@ -0,0 +1,21 @@
+"""
+Problem 9: Special Pythagorean Triplet
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000
+"""
+
+# brute force by calculating all the triples, finding onces that sum up to 1000
+
+# if a, b, c is a pythogorian triples, then ka, kb, kc for any positive k
+
+# Euclid's formula
+
+
+def solve():
+    pass
+
+
+solve()
