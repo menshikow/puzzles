4a1ededc5a640def6809316208cd789824a43e89
menshikow <adriaanmenschikow@gmail.com>
2026-06-26 22:44:55 +0200

    [leetcode] solved 283

---
+++ leetcode/283-move-zeroes/sol.py
@@ -0,0 +1,30 @@
+"""
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 104
+"""
+
+
+class Solution:
+     def moveZeroes(self, nums: list[int]) -> None:
+          insert_pos: int = 0
+          for i in range(len(nums)):
+               if nums[i] != 0:
+                    nums[insert_pos], nums[i] = nums[i], nums[insert_pos]
+                    insert_pos += 1
