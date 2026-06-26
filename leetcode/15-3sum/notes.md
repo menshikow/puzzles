d6475a5d807b76295e992263d75bb46759719977
menshikow <adriaanmenschikow@gmail.com>
2026-06-26 15:51:37 +0200

    [leetcode]: solved 3Sum

---
+++ leetcode/15-3sum/notes.md
@@ -0,0 +1,3 @@
+# Notes on the problem
+
+- Basically just upgraded 2Sum, the idea was to simplify it to the moment we basically solve 2Sum
--- leetcode/15-3sum/sol.py
+++ leetcode/15-3sum/sol.py
@@ -1,59 +1,49 @@
 # Input: nums = [-1,0,1,2,-1,-4]
 # Output: [[-1,-1,2],[-1,0,1]]
+
 # Explanation:
 # nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
 # nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 # nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 # The distinct triplets are [-1,0,1] and [-1,-1,2].
+
 # Notice that the order of the output and the order of the triplets does not matter.
 
 
-# class Solution:
-# ass much memory, but why?
-
-# def threeSum(nums: list[int]) -> list[list[int]]:
-#     sorted_nums = sorted(nums)
-#     i: int = 0
-#     sol: list[list[int]] = []
-
-#     while i < len(nums) - 2:
-#         k = i + 1
-#         j = len(nums) - 1
-
-#         while k < j:
-#             result = [sorted_nums[i] + sorted_nums[k] + sorted_nums[j]]
-#             if sum(result) == 0:
-#                 sol.append(result)
-#             elif sum(result) > 0:
-#                 j -= 1
-#             else:
-#                 k += 1
-#         i += 1
-
-#     return sol
-
-
-# test = [-1, 0, 1, 2, -1, -4]
-# threeSum(test)
-
 
 class Solution:
-    def threeSum(self, nums:list[int]):
-        nums.sort() # O(nlogn)
-        result = set()
-        n: int = len(nums)
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        """
+        Time-Complexity = O(n^2) -> 2Sum is O(n), 2Sum on every itemn => n*n = n^2
+        Space-Complexity = ??
+        """
+        nums.sort() # everytime we sort an array the complexity gets to O(nlogn)
+        n = len(nums)
+        result = []
 
         for i in range(n):
-            target: int = -nums[i]
-            left: int = i + 1
-            right: int = n - 1
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            target = -nums[i]
+            left, right = i + 1, n - 1
 
             while left < right:
-                s: int = nums[left] + nums[right] # cannot add lists to a set
+                s = nums[left] + nums[right]
+
                 if s == target:
-                    result.add([nums[i], nums[left], nums[right]])
-                if s > target:
-                    right -= 1
-                else:
+                    result.append([nums[i], nums[left], nums[right]])
                     left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif s < target:
+                    left += 1
+                else:
+                    right -= 1
+
         return result
