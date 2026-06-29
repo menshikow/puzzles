6b55a4cc90c9a0b124c9de78971f6b9992a8aef9
menshikow <adriaanmenschikow@gmail.com>
2026-06-29 18:05:35 +0200

    [leetcode]: solved 283

--- leetcode/283-move-zeroes/sol.py
+++ leetcode/283-move-zeroes/sol.py
@@ -18,10 +18,11 @@ Constraints:
     j<= nums.length <= 104
 """
 
+
 class Solution:
-     def moveZeroes(self, nums: list[int]) -> None:
-          insert_pos: int = 0
-          for i in range(len(nums)):
-               if nums[i] != 0:
-                    nums[insert_pos], nums[i] = nums[i], nums[insert_pos]
-                    insert_pos += 1
+    def moveZeroes(self, nums: list[int]) -> None:
+        insert_pos: int = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert_pos], nums[i] = nums[i], nums[insert_pos]
+                insert_pos += 1
