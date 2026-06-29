459e42f06fccc5c9adbb4780b9d072ffa31183d1
menshikow <adriaanmenschikow@gmail.com>
2026-06-29 17:48:22 +0200

    [leetcode] solved 844: Backspace String Compare

--- leetcode/643-maximum-average-subarray-1/sol.py
+++ leetcode/643-maximum-average-subarray-1/sol.py
@@ -1,8 +1,37 @@
+"""
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+
+
+
+Constraints:
+
+    n == nums.length
+    1 <= k <= n <= 105
+    -104 <= nums[i] <= 104
+
+"""
+
+
 class Solution:
     def findMaxAverage(self, nums: list[int], k: int) -> float:
-        begin = 0
-        window_state = 0
-        result = float("-inf")
+        result: float = 0.0
+        begin: int = 0
+        window_state: int = 0
 
         for end in range(len(nums)):
             window_state += nums[end]
--- leetcode/844-backstring-compare/sol.py
+++ leetcode/844-backstring-compare/sol.py
@@ -1,5 +1,67 @@
+# Problem - 844: backspace string compare
+
+
+# time: o(n), space: O(1)
 # class Solution:
 def backspaceCompare(s: str, t: str) -> bool:
-     s2: str = ""
-     t2: str = ""
-     
+    p1 = len(s) - 1
+    p2 = len(t) - 1
+
+    while p1 >= 0 or p2 >= 0:
+        # find valid character in s
+        skip = 0
+        while p1 >= 0:
+            if s[p1] == "#":
+                skip += 1
+                p1 -= 1
+            elif skip > 0:
+                skip -= 1
+                p1 -= 1
+            else:
+                break
+
+        # find valid character in t
+        skip = 0
+        while p2 >= 0:
+            if t[p2] == "#":
+                skip += 1
+                p2 -= 1
+            elif skip > 0:
+                skip -= 1
+                p2 -= 1
+            else:
+                break
+
+        # the index can be < 0 -> python evals it as reading from end
+        if p1 >= 0 and p2 >= 0:
+            if s[p1] != t[p2]:
+                return False
+        elif p1 >= 0 or p2 >= 0:
+            # one has characters other doesnt
+            return False
+
+        # move forward for to not enter infinite-loop
+        p1 -= 1
+        p2 -= 1
+
+    return True
+
+
+# test
+
+# Example 2:
+
+# Input: s = "ab##", t = "c#d#"
+# Output: true
+# Explanation: Both s and t become "".
+
+# Example 3:
+
+# Input: s = "a#c", t = "b"
+# Output: false
+# Explanation: s becomes "c" while t becomes "b".
+
+s, t = "a", "aa#a"
+# s, t = "a#c", "b"
+
+print(backspaceCompare(s, t))
