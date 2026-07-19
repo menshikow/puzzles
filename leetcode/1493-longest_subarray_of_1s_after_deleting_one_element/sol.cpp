9cfd84c4939cdd9513129f1f11e8d8f2f954f443
menshikow <adriaanmenschikow@gmail.com>
2026-07-19 22:17:31 +0200

    started solving 1493

---
+++ leetcode/1493-longest_subarray_of_1s_after_deleting_one_element/sol.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+/*
+ Given a binary array nums, you should delete one element from it.
+ Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+ Input: nums = [1,1,0,1]
+ Output: 3
+ Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+*/
+
+int main() {
+
+  return 0;
+}
